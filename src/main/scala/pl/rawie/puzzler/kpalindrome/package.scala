9a0f7cbf1901a5cba21ef2eadb5ec770a37861b0
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2013-09-19 15:03:21 +0200

    k > 0 palindrome

--- src/main/scala/pl/rawie/puzzler/kpalindrome/package.scala
+++ src/main/scala/pl/rawie/puzzler/kpalindrome/package.scala
@@ -2,9 +2,23 @@ package pl.rawie.puzzler
 
 package object kpalindrome {
   def kpalindrome(s: String, k: Int): Boolean = {
+    def indexes(s: String, c: Char): Seq[Int] =
+      s.zipWithIndex filter(_._1 == c) map(_._2)
+    def setChar(s: String, i: Int, c: Char): String = (s take i) + c + (s drop i + 1)
+
     if (s.length <= 1) true
-    else if (s.head == s.last) kpalindrome(s substring(1, s.length - 1), k
-    )
+    else if (s.head == s.last) kpalindrome(s substring(1, s.length - 1), k)
+    else if (k > 0) {
+      val inner = s substring(1, s.length - 1)
+      val changes: List[(Char, Char)] = List((s.head, s.last), (s.last, s.head))
+      val perms: List[String] = {
+        for ((from, to) <- changes; i <- indexes(inner, from))
+        yield setChar(inner, i, to)
+      }
+      perms
+        .map(t => kpalindrome(t, k - 1))
+        .foldRight(false)(_ || _)
+    }
     else false
   }
 }
--- src/test/scala/pl/rawie/puzzler/kpalindrome/KPalindromeSpec.scala
+++ src/test/scala/pl/rawie/puzzler/kpalindrome/KPalindromeSpec.scala
@@ -22,4 +22,54 @@ class KPalindromeSpec extends FunSpec with ShouldMatchers {
       kp("abcab") should equal(false)
     }
   }
+
+  describe("kpalindrome (k = 1)") {
+    def kp(s: String): Boolean = kpalindrome(s, 1)
+
+    it("should return true for palindrome") {
+      kp("") should equal(true)
+      kp("a") should equal(true)
+      kp("aa") should equal(true)
+      kp("aba") should equal(true)
+      kp("abba") should equal(true)
+      kp("abcba") should equal(true)
+    }
+
+    it("should return true for one step palindrome") {
+      kp("aab") should equal(true)
+      kp("abab") should equal(true)
+      kp("abcab") should equal(true)
+    }
+
+    it("should return false for two step palindrome") {
+      kp("baabc") should equal(false)
+    }
+  }
+
+  describe("kpalindrome (k = 2)") {
+    def kp(s: String): Boolean = kpalindrome(s, 2)
+
+    it("should return true for palindrome") {
+      kp("") should equal(true)
+      kp("a") should equal(true)
+      kp("aa") should equal(true)
+      kp("aba") should equal(true)
+      kp("abba") should equal(true)
+      kp("abcba") should equal(true)
+    }
+
+    it("should return true for one step palindrome") {
+      kp("aab") should equal(true)
+      kp("abab") should equal(true)
+      kp("abcab") should equal(true)
+    }
+
+    it("should return false for two step palindrome") {
+      kp("baabc") should equal(true)
+    }
+
+    it("should return false for three step palindrome") {
+      kp("aaccdedbb") should equal(false)
+    }
+  }
 }
