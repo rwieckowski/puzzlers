39d3964dd6e3ff36e67a89612a8eb8da416492aa
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2013-09-19 13:49:41 +0200

    k=0 palindrome

--- src/main/scala/pl/rawie/puzzler/kpalindrome/package.scala
+++ src/main/scala/pl/rawie/puzzler/kpalindrome/package.scala
@@ -1,5 +1,10 @@
 package pl.rawie.puzzler
 
 package object kpalindrome {
-  def kpalindrome(s: String, k: Int): Boolean = ???
+  def kpalindrome(s: String, k: Int): Boolean = {
+    if (s.length <= 1) true
+    else if (s.head == s.last) kpalindrome(s substring(1, s.length - 1), k
+    )
+    else false
+  }
 }
--- src/test/scala/pl/rawie/puzzler/kpalindrome/KPalindromeSpec.scala
+++ src/test/scala/pl/rawie/puzzler/kpalindrome/KPalindromeSpec.scala
@@ -4,9 +4,22 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSpec
 
 class KPalindromeSpec extends FunSpec with ShouldMatchers {
-  describe("kpalindrome") {
-    it("should") {
-      pending
+  describe("kpalindrome (k = 0)") {
+    def kp(s: String): Boolean = kpalindrome(s, 0)
+
+    it("should return true for palindrome") {
+      kp("") should equal(true)
+      kp("a") should equal(true)
+      kp("aa") should equal(true)
+      kp("aba") should equal(true)
+      kp("abba") should equal(true)
+      kp("abcba") should equal(true)
+    }
+
+    it("should return false for one step palindrome") {
+      kp("aab") should equal(false)
+      kp("abab") should equal(false)
+      kp("abcab") should equal(false)
     }
   }
 }
